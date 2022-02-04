cd19936e6834c42cd9ccf9a5c1952d588832fd25
Namdev Patil <pythonsessions66@gmail.com>
2022-02-04 10:22:16 +0530

    first commit

---
+++ 04-02-2022/src/com/javaDataTypes/Example1.java
@@ -0,0 +1,22 @@
+package com.javaDataTypes;
+
+public class Example1
+{
+
+	public static void main(String[] args)
+	{
+		/**
+		 * write a java program to print customer information(id, name and address)
+		 */
+		
+		int customerId = 7886;
+		String customerName = "Nikitha Reddy";
+		String customerAddress = "Hyderabad";
+		
+		System.out.println(customerId);
+		System.out.println(customerName);
+		System.out.println(customerAddress);
+		
+	}
+
+}
